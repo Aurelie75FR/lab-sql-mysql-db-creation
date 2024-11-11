2e56de988900e9b2e9013d835f86b4a9222a8baa
Aurelie75FR <aurelielo.vi@gmail.com>
2024-11-11 14:03:37 +0100

    lab solved + bonus

--- create.sql
+++ create.sql
@@ -1,36 +1,52 @@
-CREATE TABLE `invoices`(
-    `invoice_num` INT UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    `car` INT NOT NULL,
-    `date` DATE NOT NULL,
-    `customer` INT NOT NULL,
-    `salesperson` INT NOT NULL
-);
+CREATE DATABASE IF NOT EXISTS lab_mysql;
+
+USE lab_mysql;
+
+DROP TABLE IF EXISTS salespersons;
 CREATE TABLE `salespersons`(
-    `staff_id` INT UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    `name` CHAR(255) NOT NULL,
-    `store` CHAR(255) NOT NULL
+    `id` INT NOT NULL,
+    `staff_id` INT UNSIGNED NOT NULL AUTO_INCREMENT,
+    `name` VARCHAR(20) NOT NULL,
+    `store` VARCHAR(20) NOT NULL,
+    PRIMARY KEY (`staff_id`)
 );
+
+DROP TABLE IF EXISTS customers;
 CREATE TABLE `customers`(
-    `customer_id` INT UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    `name` CHAR(255) NOT NULL,
-    `phone_num` INT NOT NULL,
-    `email` CHAR(255) NOT NULL,
-    `adress` CHAR(255) NOT NULL,
-    `city` CHAR(255) NOT NULL,
-    `state_province` CHAR(255) NOT NULL,
-    `country` CHAR(255) NOT NULL,
-    `zip_postal` INT NOT NULL
+    `id` INT NOT NULL,
+    `customer_id` INT UNSIGNED NOT NULL AUTO_INCREMENT,
+    `name` VARCHAR(20) NOT NULL,
+    `phone_num` VARCHAR(30) NOT NULL,
+    `email` VARCHAR(50) NOT NULL,
+    `adress` VARCHAR(100) NOT NULL,
+    `city` VARCHAR(50) NOT NULL,
+    `state_province` VARCHAR(50) NOT NULL,
+    `country` VARCHAR(50) NOT NULL,
+    `zip_postal` VARCHAR(15) NOT NULL,
+    PRIMARY KEY (`customer_id`)
 );
+
+DROP TABLE IF EXISTS cars;
 CREATE TABLE `cars`(
-    `VIN` INT UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    `manufacturer` CHAR(255) NOT NULL,
-    `model` CHAR(255) NOT NULL,
-    `year` INT NOT NULL,
-    `color` CHAR(255) NOT NULL
+    `id` INT NOT NULL,
+    `VIN` VARCHAR(17) NOT NULL,
+    `manufacturer` VARCHAR(50) NOT NULL,
+    `model` VARCHAR(50) NOT NULL,
+    `year` YEAR NOT NULL,
+    `color` VARCHAR(20) NOT NULL,
+    PRIMARY KEY (`VIN`)
 );
-ALTER TABLE
-    `customers` ADD CONSTRAINT `customers_name_foreign` FOREIGN KEY(`name`) REFERENCES `invoices`(`customer`);
-ALTER TABLE
-    `invoices` ADD CONSTRAINT `invoices_car_foreign` FOREIGN KEY(`car`) REFERENCES `cars`(`VIN`);
-ALTER TABLE
-    `invoices` ADD CONSTRAINT `invoices_salesperson_foreign` FOREIGN KEY(`salesperson`) REFERENCES `salespersons`(`staff_id`);
+
+DROP TABLE IF EXISTS invoices;
+CREATE TABLE `invoices`(
+    `id` INT NOT NULL,
+    `invoice_number` INT NOT NULL,
+    `invoice_date` DATE NOT NULL,
+    `car` VARCHAR(17) NOT NULL,
+    `customer` INT UNSIGNED NOT NULL,
+    `salesperson` INT UNSIGNED NOT NULL,
+    PRIMARY KEY (`invoice_number`),
+    FOREIGN KEY (`customer`) REFERENCES `customers`(`customer_id`),
+    FOREIGN KEY (`car`) REFERENCES `cars`(`VIN`),
+    FOREIGN KEY (`salesperson`) REFERENCES `salespersons`(`staff_id`)
+);
